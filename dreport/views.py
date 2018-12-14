2a472bb98d3e8c42d42d084bb762b6675cb4557d
zhangweijie <zhangweijie@wtsd.cn>
2018-12-14 15:03:55 +0800

    test month view

--- dreport/views.py
+++ dreport/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from datetime import datetime
 from .models.city import CityMonthRecord, City, CityPauseRecord
 from common.permissions import AdminUserRequiredMixin
 # Create your views here.
@@ -21,7 +22,11 @@ class CityMonthView(AdminUserRequiredMixin, ListView):
     # model = CityMonthRecord
     template_name = 'dreport/city_month.html'
     context_object_name = 'city_month'
-    queryset = CityMonthRecord.objects.filter(month=10)
+
+    def get_queryset(self):
+        month = self.request.GET.get('month') if self.request.GET.get('month') else datetime.strftime(datetime.now(), '%m')
+        queryset = CityMonthRecord.objects.filter(month=month)
+        return queryset
 
 
 class CityRecord(AdminUserRequiredMixin, ListView):
