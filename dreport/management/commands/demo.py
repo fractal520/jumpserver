54977547510a37fbe296b4a7d944fb4833a10013
zhangweijie <zhangweijie@wtsd.cn>
2018-12-17 17:30:55 +0800

    test command

--- dreport/management/commands/demo.py
+++ dreport/management/commands/demo.py
@@ -9,12 +9,12 @@ from assets.models import Asset
 class Command(BaseCommand):
 
     def __init__(self):
-        self.rcs_ip = '10.10.3.46'
+        self.rcs_ip = '192.168.0.127'
         self.rcs_log_dir = '/scss/logs/rcs/'
 
     def handle(self, *args, **options):
         yestarday = datetime.strftime(datetime.now() - timedelta(days=1), "%Y-%m-%d")
         log_path = os.path.join(self.rcs_log_dir, 'rcs.', yestarday, '.log')
-        rcs = Asset.objects.get(ip='10.10.3.46')
+        rcs = Asset.objects.get(ip=self.rcs_ip)
         print(rcs.admin_user)
         print("hello world!")
