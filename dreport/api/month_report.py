bcaec1f3a5e0db1c28a4007923e2f55991d619a8
zhangweijie <zhangweijie@wtsd.cn>
2018-12-20 13:22:50 +0800

    test filter.

--- dreport/api/month_report.py
+++ dreport/api/month_report.py
@@ -9,10 +9,9 @@ from django.http import JsonResponse
 
 
 def create_month_record(request):
-    data = request.GET
     print(request.POST)
-    city_id = data.get("id", None)
-    date = data.get("record-month", None)
+    city_id = request.POST.get("id", None)
+    date = request.POST.get("record-month", None)
     bot = MonthRecordFunction()
     bot.create(city_id=city_id, date=date)
     return JsonResponse(dict(code=200, msg=''))
