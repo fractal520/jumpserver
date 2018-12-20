67720878bcdb2a1169a631bf428c639eb6c42c89
zhangweijie <zhangweijie@wtsd.cn>
2018-12-20 11:44:01 +0800

    add filter.

--- dreport/api/month_report.py
+++ dreport/api/month_report.py
@@ -4,13 +4,15 @@
 from rest_framework import generics
 from common.permissions import IsOrgAdmin, IsOrgAdminOrAppUser
 from ..models.city import City, CityMonthRecord
+from ..utils import *
 from django.http import JsonResponse
 
 
 def create_month_record(request):
     data = request.GET
-    print(request.body)
     print(request.POST)
-    # print(data.get("id"))
-    # print(data.get("record-month"))
+    city_id = data.get("id", None)
+    date = data.get("record-month", None)
+    bot = MonthRecordFunction()
+    bot.create(city_id=city_id, date=date)
     return JsonResponse(dict(code=200, msg=''))
---
+++ dreport/utils.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+
+from .models.city import City, CityMonthRecord, CityPauseRecord
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class MonthRecordFunction(object):
+
+    def create(self, city_id, date):
+        try:
+            city = City.objects.get(id=city_id)
+        except ObjectDoesNotExist as error:
+            print(error)
+            return False
+        print(date)
+        # CityPauseRecord.objects.filter(risk_date__month=12, risk_date__year=2018)
+        return True
