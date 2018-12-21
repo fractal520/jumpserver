ec1a80b1e3f1da04f666526a597b9c0f67acbbbd
zhangweijie <zhangweijie@wtsd.cn>
2018-12-21 16:19:33 +0800

    test download function

--- dreport/api/month_report.py
+++ dreport/api/month_report.py
@@ -44,6 +44,6 @@ def download_report(request, pk):
     file_path = os.path.join(settings.DEVICE_REPORT_DIR, file)
     print(file_path)
     response = FileResponse(open(file_path, 'rb'))
-    response['Content-Type'] = 'application/octet-stream'
-    response['Content-Disposition'] = 'attachment;filename="{0}"'.format(file)
+    # response['Content-Type'] = 'application/octet-stream'
+    # response['Content-Disposition'] = 'attachment;filename="{0}"'.format(file)
     return response
