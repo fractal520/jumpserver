d20d7afa309a9174f991368aec7098a381a9285d
zhangweijie <zhangweijie@wtsd.cn>
2018-12-21 16:32:12 +0800

    test download function

--- dreport/api/month_report.py
+++ dreport/api/month_report.py
@@ -45,6 +45,6 @@ def download_report(request, pk):
     print(file_path)
     response = FileResponse(open(file_path, 'rb'))
     # response['Content-Type'] = 'application/octet-stream'
-    response['Content-Type'] = 'text/html'
+    response['Content-Type'] = 'application/msword'
     response['Content-Disposition'] = 'attachment;filename="{0}"'.format(file)
     return response
