cafb293bf66a80e99eaa0f52dc72717173810bc7
zhangweijie <zhangweijie@wtsd.cn>
2018-12-24 09:43:03 +0800

    city update function优化显示

--- dreport/views.py
+++ dreport/views.py
@@ -30,7 +30,7 @@ class CityUpdateView(AdminUserRequiredMixin, UpdateView):
             'action': _('Update City'),
         }
         kwargs.update(context)
-        print(self.request)
+        print(self.request.GET)
         return super().get_context_data(**kwargs)
 
 
