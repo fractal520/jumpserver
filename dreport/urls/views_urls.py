07a03c242fe5017c59130e5988ce24161c45d6b8
zhangweijie <zhangweijie@wtsd.cn>
2018-12-24 13:21:12 +0800

    月度视图删除功能

--- dreport/urls/views_urls.py
+++ dreport/urls/views_urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('CityView/<uuid:pk>/update/', views.CityUpdateView.as_view(), name='city-update'),
     path('CityView/create/', views.CityCreateView.as_view(), name='city-create'),
     path('CityMonthView/', views.CityMonthView.as_view(), name='CityMonthView'),
-    # path('CityMonthView/<uuid:pk>/delete/', views.CityMonthDeleteView.as_view(), name='CityMonthDeleteView'),
+    path('CityMonthView/<uuid:pk>/delete/', views.CityMonthDeleteView.as_view(), name='CityMonthDeleteView'),
     path('CityRecord/', views.CityRecord.as_view(), name='CityRecord'),
     path('CityRecord/<uuid:pk>/update/', views.RecordUpdateView.as_view(), name='record-update'),
 ]
