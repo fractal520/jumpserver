515e1f3608c8dbc4baae3c78caabdac5a3f782e3
zhangweijie <zhangweijie@wtsd.cn>
2018-12-28 09:06:44 +0800

    回滚脚本修改，回滚时删除lib和config文件夹，从备份全量恢复过来

--- deploy/script/unpack.sh
+++ deploy/script/unpack.sh
@@ -7,4 +7,13 @@ app_name=$3
 cd ${backup_directory}
 
 tar -xvzf ${backup_path}
-cp -rf ${backup_directory}/data/${app_name}/config/* /data/${app_name}/config/
+if [ -d /data/${app_name}/config ];then
+    cp -rf ${backup_directory}/data/${app_name}/config/* /data/${app_name}/config/
+else
+    cp -rf ${backup_directory}/data/${app_name}/config /data/${app_name}/
+fi
+
+if [ -d /data/${app_name}/lib ];then
+    rm -rf /data/${app_name}/lib
+fi
+cp -rf ${backup_directory}/data/${app_name}/lib /data/${app_name}/
