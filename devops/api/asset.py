6af5a0f426c7c946c30a3724d3f36a1d9a8abc00
zhangweijie <zhangweijie@wtsd.cn>
2019-01-18 13:37:30 +0800

    devops api-url

--- devops/api/asset.py
+++ devops/api/asset.py
@@ -1,57 +1,43 @@
-from rest_framework_bulk import BulkModelViewSet
-from rest_framework.pagination import LimitOffsetPagination
 from django.shortcuts import get_object_or_404
-from django.db.models import Q
-from assets import serializers
-from assets.utils import LabelFilter
-from common.mixins import IDInFilterMixin
-from common.permissions import IsOrgAdminOrAppUser
-from assets.models import Asset, Node, AdminUser
+from rest_framework.generics import ListAPIView
+from assets.serializers import AssetGrantedSerializer
+from users.models import User
+from perms.utils import AssetPermissionUtil
+from common.permissions import IsOrgAdminOrAppUser, IsValidUser
+from orgs.utils import set_to_root_org
 
 
-class AssetViewSet(IDInFilterMixin, LabelFilter, BulkModelViewSet):
+class UserGrantedAssetsApi(ListAPIView):
     """
-    API endpoint that allows Asset to be viewed or edited.
+    用户授权的所有资产
     """
-    filter_fields = ("hostname", "ip")
-    search_fields = filter_fields
-    ordering_fields = ("hostname", "ip", "port", "comment")
-    queryset = Asset.objects.all()
-    serializer_class = serializers.AssetSerializer
-    pagination_class = LimitOffsetPagination
     permission_classes = (IsOrgAdminOrAppUser,)
+    serializer_class = AssetGrantedSerializer
 
-    def filter_node(self):
-        node_id = self.request.query_params.get("node_id")
-        if not node_id:
-            return
-
-        node = get_object_or_404(Node, id=node_id)
-        show_current_asset = self.request.query_params.get("show_current_asset") in ('1', 'true')
-
-        if node.is_root():
-            if show_current_asset:
-                self.queryset = self.queryset.filter(
-                    Q(nodes=node_id) | Q(nodes__isnull=True)
-                ).distinct()
-            return
-        if show_current_asset:
-            self.queryset = self.queryset.filter(nodes=node).distinct()
-        else:
-            self.queryset = self.queryset.filter(
-                nodes__key__regex='^{}(:[0-9]+)*$'.format(node.key),
-            ).distinct()
-
-    def filter_admin_user_id(self):
-        admin_user_id = self.request.query_params.get('admin_user_id')
-        if admin_user_id:
-            admin_user = get_object_or_404(AdminUser, id=admin_user_id)
-            self.queryset = self.queryset.filter(admin_user=admin_user)
+    def change_org_if_need(self):
+        if self.request.user.is_superuser or \
+                self.request.user.is_app or \
+                self.kwargs.get('pk') is None:
+            set_to_root_org()
 
     def get_queryset(self):
-        self.queryset = super().get_queryset()\
-            .prefetch_related('labels', 'nodes')\
-            .select_related('admin_user')
-        self.filter_admin_user_id()
-        self.filter_node()
-        return self.queryset.distinct()
+        self.change_org_if_need()
+        user_id = self.kwargs.get('pk', '')
+        queryset = []
+
+        if user_id:
+            user = get_object_or_404(User, id=user_id)
+        else:
+            user = self.request.user
+
+        util = AssetPermissionUtil(user)
+        for k, v in util.get_assets().items():
+            system_users_granted = [s for s in v if s.protocol == k.protocol]
+            k.system_users_granted = system_users_granted
+            queryset.append(k)
+        return queryset
+
+    def get_permissions(self):
+        if self.kwargs.get('pk') is None:
+            self.permission_classes = (IsValidUser,)
+        return super().get_permissions()
