a21c1a5e12958ce4422adb7f043b59affaa3b131
zhangweijie <zhangweijie@wtsd.cn>
2019-02-13 13:47:50 +0800

    DEVOPS add routing inspection.

---
+++ devops/management/commands/RoutingInspection.py
@@ -0,0 +1,49 @@
+# !/usr/bin/env python
+# encoding: utf-8
+from datetime import datetime
+from django.core.management.base import BaseCommand
+from celery import shared_task
+from common.utils import get_logger
+from assets import const
+from assets.models import Assets
+
+logger = get_logger('jumpserver')
+DATE_TIME = datetime.now()
+
+
+@shared_task
+def get_asset_hardware_info(assets):
+    task_name = ("Daily routing inspection.Date: {}".format(DATE_TIME.strftime("%Y%m%d")))
+    logger.info(task_name)
+    return get_asset_hardware_info_util(assets=assets, task_name=task_name)
+
+
+@shared_task
+def get_asset_hardware_info_util(task_name, assets):
+    from ops.utils import update_or_create_ansible_task
+    hosts = [asset.fullname for asset in assets if asset.is_active and asset.is_unixlike()]
+    tasks = const.UPDATE_ASSETS_HARDWARE_TASKS
+
+    task, created = update_or_create_ansible_task(
+        task_name, hosts=hosts, tasks=tasks, pattern='all',
+        options=const.TASK_OPTIONS, run_as_admin=True, created_by='System',
+    )
+
+    result = task.run()
+
+    genrate_routing_record(result)
+    logger.debug(result)
+    return result
+
+
+@shared_task
+def genrate_routing_record(result):
+    pass
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        assets = Assets.objects.all()
+        result = get_asset_hardware_info(assets=assets)
+        print(result)
