f22ce108ddc6fc8f811a340d64a158d7181d8202
zhangweijie <zhangweijie@wtsd.cn>
2019-02-18 15:49:20 +0800

    增加轮询查询jenkins功能:测试

--- deploy/api/polling.py
+++ deploy/api/polling.py
@@ -1,13 +1,48 @@
 # encoding: utf-8
 
+from time import sleep
 from ..pjenkins.exec_jenkins import JenkinsWork
 from common.utils import get_logger
 
 logger = get_logger('jumpserver')
 
 
-def polling(request):
+def first_polling(request):
     jw = JenkinsWork()
-    result = jw.collect_job(name='MPS')
-    # print(result)
-    return result
+    MAX_COUNT = 100
+    polling_count = 0
+    while polling_count < MAX_COUNT:
+        print(polling_count)
+        result = jw.collect_job(name='MPS')
+
+        if result['build_status'] == "SUCCESS":
+            logger.debug('polling build_status SUCCESS continue')
+        if result['build_status'] == "RUNNING":
+            return "RUNNING"
+        if result['build_status'] == "FAILURE":
+            return "FAILURE"
+        polling_count += 1
+        sleep(2)
+
+    return "TIME OUT FAILURE"
+
+
+def sec_polling(request):
+    jw = JenkinsWork()
+    MAX_COUNT = 100
+    polling_count = 0
+    while polling_count < MAX_COUNT:
+        print(polling_count)
+        result = jw.collect_job(name='MPS')
+
+        if result['build_status'] == "SUCCESS":
+            return "SUCCESS"
+        if result['build_status'] == "RUNNING":
+            logger.debug('polling build_status RUNNING continue')
+        if result['build_status'] == "FAILURE":
+            return "FAILURE"
+        polling_count += 1
+        sleep(2)
+
+    return "TIME OUT FAILURE"
+
